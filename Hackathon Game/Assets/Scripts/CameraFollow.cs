7c60f899399751bb0425b157492d23c120b702eb
Jeremy Brown <jj.brown@hotmail.com>
2015-10-10 14:34:24 -0400

    Added camera tracking

---
+++ Hackathon Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow: MonoBehaviour {
+	public Transform target;
+
+	void LateUpdate (){
+
+		float speed = 1f;
+		float smooth = 1.0f - Mathf.Pow (0.5f, Time.deltaTime * speed);
+		Vector3 fakedPlayerPos = new Vector3 (target.position.x, target.position.y + 2, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, fakedPlayerPos, smooth);
+	}
+} 
