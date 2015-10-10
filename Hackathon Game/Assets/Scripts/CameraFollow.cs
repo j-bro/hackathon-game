78c640b9084630959801aa058c8c6b89d4040858
Jeremy Brown <jj.brown@hotmail.com>
2015-10-10 15:16:24 -0400

    cam follow

--- Hackathon Game/Assets/Scripts/CameraFollow.cs
+++ Hackathon Game/Assets/Scripts/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow: MonoBehaviour {
 
 	void LateUpdate (){
 
-		float speed = 1f;
+		float speed = 5f;
 		float smooth = 1.0f - Mathf.Pow (0.5f, Time.deltaTime * speed);
 		Vector3 fakedPlayerPos = new Vector3 (target.position.x, target.position.y + 2, transform.position.z);
 		transform.position = Vector3.Lerp (transform.position, fakedPlayerPos, smooth);
