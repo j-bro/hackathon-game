8ce43673ea834a76bbfc6d079bc806a0167ef73a
Jeremy Brown <jj.brown@hotmail.com>
2015-10-10 15:06:16 -0400

    Added player control

---
+++ Hackathon Game/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //set the base player speed to the screen scroll rate
+           // float xSpeed = 50;
+           // Vector2 currentSpeed = GetComponent<Rigidbody2D>().velocity;
+
+            //set the player's speed
+           // if (currentSpeed.x < xSpeed)
+                GetComponent<Rigidbody2D>().AddForce(Vector2.right * 50f, ForceMode2D.Force);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * 50f, ForceMode2D.Force);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.LogWarning("right key was pressed");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.LogWarning("down key was pressed");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.LogWarning("space was pressed");
+        }
+    }
+}
